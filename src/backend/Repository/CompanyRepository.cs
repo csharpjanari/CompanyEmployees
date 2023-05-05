8999f3dd2d64205c6246a5e43ad18711fba1bbf7
Raul Janari <rdjanari1707@gmail.com>
2023-05-05 16:43:07 +0200

    Async Repository

--- src/backend/Repository/CompanyRepository.cs
+++ src/backend/Repository/CompanyRepository.cs
@@ -1,5 +1,6 @@
 using Contracts;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository;
 
@@ -9,18 +10,18 @@ public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
     {
     }
 
-    public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
-        FindAll(trackChanges)
+    public async Task<IEnumerable<Company>> GetAllCompanies(bool trackChanges) =>
+        await FindAll(trackChanges)
             .OrderBy(c => c.Name)
-            .ToList();
+            .ToListAsync();
 
-    public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) => 
-        FindByCondition(x => ids.Contains(x.Id), trackChanges)
-            .ToList();
+    public async Task<IEnumerable<Company>> GetByIds(IEnumerable<Guid> ids, bool trackChanges) => 
+        await FindByCondition(x => ids.Contains(x.Id), trackChanges)
+            .ToListAsync();
 
-    public Company GetCompany(Guid companyId, bool trackChanges) =>
-        FindByCondition(c => c.Id.Equals(companyId), trackChanges)
-            .SingleOrDefault();
+    public async Task<Company> GetCompany(Guid companyId, bool trackChanges) =>
+        await FindByCondition(c => c.Id.Equals(companyId), trackChanges)
+            .SingleOrDefaultAsync();
 
     public void CreateCompany(Company company) => Create(company);
 
