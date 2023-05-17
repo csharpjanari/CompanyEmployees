ec2595df5ca416b6ff12347abba404bddf34fb89
Raul Janari <rdjanari1707@gmail.com>
2023-05-17 22:19:32 +0200

    Deprecated version

--- src/backend/Presentation/Controllers/V2/CompaniesV2Controller.cs
+++ src/backend/Presentation/Controllers/V2/CompaniesV2Controller.cs
@@ -3,7 +3,7 @@ using Service.Contracts;
 
 namespace Presentation.Controllers.V2;
 
-[ApiVersion("2.0")]
+[ApiVersion("2.0", Deprecated = true)]
 [Route("api/companies")]
 [ApiController]
 public class CompaniesV2Controller : ControllerBase
